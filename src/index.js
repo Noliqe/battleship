4b1cc3123396f007fe3b2bb8510c2558caa66f73
Noliqe <tournoliqe1@gmail.com>
2022-09-21 18:58:12 +0200

    add gameboard function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 /* eslint-disable no-plusplus */
 // import './style.css';
 
-const ship = (shipName, shipLength, damage, shipSunk) => {
-  const shipArray = [];
-  for (let i = 0; i < shipLength; i++) {
-    shipArray.push(i);
-  }
+const ship = (shipName, shipLength, damage, shipSunk, coordinates) => {
   const object = {
     name: shipName,
     length: shipLength,
     damaged: damage,
     sunk: shipSunk,
-    array: shipArray,
+    array: coordinates,
   };
 
   const isSunk = () => {
@@ -34,4 +30,103 @@ const ship = (shipName, shipLength, damage, shipSunk) => {
   };
 };
 
-module.exports = { ship };
+const gameboard = () => {
+  // create gameboardGrid *
+  const gameArray = [];
+  for (let i = 0; i < 100; i++) {
+    gameArray.push('');
+  }
+  // make new ship using factorial function
+  const carrier = ship('carrier', 5, '', false, [1, 2, 3, 4, 5]);
+  const battleship = ship('battleship', 4, '', false, [6, 16, 26, 36]);
+  const destroyer = ship('destroyer', 3, '', false, [90, 91, 92]);
+  const submarine = ship('submarine', 3, '', false, [50, 60, 70]);
+  const patrolBoat = ship('patrol-boat', 2, '', false, [39, 49]);
+
+  // place ship in gameArray
+  const placeShip = (shipName) => {
+    if (shipName === 'carrier') {
+      for (let i = 0; i < carrier.object.array.length; i++) {
+        const index = carrier.object.array[i];
+        gameArray[index] = carrier.object.name;
+      }
+    } else if (shipName === 'battleship') {
+      for (let i = 0; i < battleship.object.array.length; i++) {
+        const index = battleship.object.array[i];
+        gameArray[index] = battleship.object.name;
+      }
+    } else if (shipName === 'destroyer') {
+      for (let i = 0; i < destroyer.object.array.length; i++) {
+        const index = destroyer.object.array[i];
+        gameArray[index] = destroyer.object.name;
+      }
+    } else if (shipName === 'submarine') {
+      for (let i = 0; i < submarine.object.array.length; i++) {
+        const index = submarine.object.array[i];
+        gameArray[index] = submarine.object.name;
+      }
+    } else if (shipName === 'patrol-boat') {
+      for (let i = 0; i < patrolBoat.object.array.length; i++) {
+        const index = patrolBoat.object.array[i];
+        gameArray[index] = patrolBoat.object.name;
+      }
+    }
+  };
+  // when receiving attack, check wether hit or not
+  const receiveAttack = (coords, num) => {
+    if (coords === 'carrier') {
+      carrier.hit(num);
+    } else if (coords === 'battleship') {
+      battleship.hit(num);
+    } else if (coords === 'destroyer') {
+      destroyer.hit(num);
+    } else if (coords === 'submarine') {
+      submarine.hit(num);
+    } else if (coords === 'patrol-boat') {
+      patrolBoat.hit(num);
+    } else {
+      // let num = txt.replace(/\D/g,'');
+      gameArray[num] = 'x';
+    }
+  };
+
+  // *
+  const gameOver = () => {
+    const a = carrier.object.sunk;
+    const b = battleship.object.sunk;
+    const c = destroyer.object.sunk;
+    const d = submarine.object.sunk;
+    const e = patrolBoat.object.sunk;
+    if (a === true && b === true && c === true && d === true && e === true) {
+      return (true);
+    }
+    return (false);
+  };
+
+  return {
+    gameArray,
+    carrier,
+    battleship,
+    destroyer,
+    submarine,
+    patrolBoat,
+    placeShip,
+    receiveAttack,
+    gameOver,
+  };
+};
+
+const player = (data, enemy) => {
+  data = gameboard();
+
+  const hitEnemy = (coords, num) => {
+    enemy.gameboard.receiveAttack(coords, num);
+  };
+
+  return {
+    data,
+    hitEnemy,
+  };
+};
+
+module.exports = { ship, gameboard };
--- src/index.test.js
+++ src/index.test.js
@@ -1,18 +1,87 @@
 /* eslint-disable */
-import {ship} from './index';
+import {ship, gameboard} from './index';
 
 test('hit', () => {
-  const carrier = ship('carrier', 5, '', false);
+  const carrier = ship('carrier', 5, '', false, []);
   carrier.hit(1);
   expect(carrier.object.damaged).toBe('1');
 });
 
 test('isSunk', () => {
-  const carrier = ship('carrier', 5, '', false);
+  const carrier = ship('carrier', 5, '', false, []);
   carrier.hit(0);
   carrier.hit(1);
   carrier.hit(2);
   carrier.hit(3);
   carrier.hit(4);
   expect(carrier.object.sunk).toBe(true);
+});
+
+test('isSunk', () => {
+  const battleship = ship('battleship', 4, '', false, []);
+  battleship.hit(0);
+  battleship.hit(1);
+  battleship.hit(2);
+  battleship.hit(3);
+  expect(battleship.object.sunk).toBe(true);
+});
+
+test('gameArray', () => {
+  const player = gameboard();
+  player.placeShip('battleship');
+  expect(player.gameArray[6]).toBe('battleship');
+});
+
+test('gameArray', () => {
+  const player = gameboard();
+  player.placeShip('battleship');
+  expect(player.gameArray[0]).toBe('');
+});
+
+test('receiveAttack', () => {
+  const player = gameboard();
+  player.placeShip('patrol-boat');
+  player.receiveAttack(player.gameArray[49], 49);
+  expect(player.patrolBoat.object.damaged).toBe('49');
+});
+
+test('receiveAttack', () => {
+  const player = gameboard();
+  player.placeShip('patrol-boat');
+  player.receiveAttack(player.gameArray[49], 49);
+  expect(player.patrolBoat.object.array).toStrictEqual([39]);
+});
+
+test('receiveAttack', () => {
+  const player = gameboard();
+  player.placeShip('submarine');
+  player.receiveAttack(player.gameArray[0], 0);
+  expect(player.gameArray[0]).toBe('x');
+});
+
+test('gameOver', () => {
+  const player = gameboard();
+  player.placeShip('submarine');
+  player.placeShip('carrier');
+  player.placeShip('battleship');
+  player.placeShip('patrol-boat');
+  player.placeShip('destroyer');
+  player.receiveAttack(player.gameArray[49], 49);
+  player.receiveAttack(player.gameArray[39], 39);
+  player.receiveAttack(player.gameArray[1], 1);
+  player.receiveAttack(player.gameArray[2], 2);
+  player.receiveAttack(player.gameArray[3], 3);
+  player.receiveAttack(player.gameArray[4], 4);
+  player.receiveAttack(player.gameArray[5], 5);
+  player.receiveAttack(player.gameArray[6], 6);
+  player.receiveAttack(player.gameArray[16], 16);
+  player.receiveAttack(player.gameArray[26], 26);
+  player.receiveAttack(player.gameArray[36], 36);
+  player.receiveAttack(player.gameArray[90], 90);
+  player.receiveAttack(player.gameArray[91], 91);
+  player.receiveAttack(player.gameArray[92], 92);
+  player.receiveAttack(player.gameArray[50], 50);
+  player.receiveAttack(player.gameArray[60], 60);
+  player.receiveAttack(player.gameArray[70], 70);
+  expect(player.gameOver()).toBe(true);
 });
