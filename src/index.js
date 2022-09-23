1562c61d06f695b16c3688a65cdd0129752fb33f
Noliqe <tournoliqe1@gmail.com>
2022-09-23 18:51:10 +0200

    save

--- src/index.js
+++ src/index.js
@@ -40,6 +40,7 @@ const gameboard = () => {
   const gameArray = [];
   const createGrid = (num) => {
     const gridBoard = document.querySelector(`.gridBoard${num}`);
+    const winner = document.querySelector('.winner');
     for (let i = 0; i < 100; i++) {
       gameArray.push('');
       const square = document.createElement('div');
@@ -48,7 +49,7 @@ const gameboard = () => {
       const x = `square${i}-board${num}`;
       square.setAttribute('id', x);
       square.addEventListener('click', () => {
-        if (num === 2) {
+        if (num === 2 && winner.textContent !== 'you lost') {
           // eslint-disable-next-line no-use-before-define
           newPlayer.hitComputer(gameArray[i], i, 2);
           square.style.pointerEvents = 'none';
@@ -56,12 +57,98 @@ const gameboard = () => {
       });
     }
   };
+
+  const shipArray = [];
+
+  const randomCoords = (num) => {
+    const row1 = [6, 16, 26, 36, 46, 56, 66, 76, 86, 96];
+    const row2 = [7, 17, 27, 37, 47, 57, 67, 77, 87, 97];
+    const row3 = [8, 18, 28, 38, 48, 58, 68, 78, 88, 98];
+    const row4 = [9, 19, 29, 39, 49, 59, 69, 79, 89, 99];
+    const column1 = [60, 61, 62, 63, 64, 65, 66, 67, 68, 69];
+    const column2 = [70, 71, 72, 73, 74, 75, 76, 77, 78, 79];
+    const column3 = [80, 81, 82, 83, 84, 85, 86, 87, 88, 89];
+    const column4 = [90, 91, 92, 93, 94, 95, 96, 97, 98, 99];
+    const randomArray = [];
+    const y = Math.floor(Math.random() * 101);
+    const x = `${y}`;
+    console.log(shipArray.includes(y));
+    const z = Math.floor(Math.random() * 2);
+    if (z === 0) {
+      const x1 = y + 1;
+      const x2 = y + 2;
+      const x3 = y + 3;
+      const x4 = y + 4;
+      // eslint-disable-next-line max-len
+      if (shipArray.includes(x) || shipArray.includes(x1) || shipArray.includes(x2) || shipArray.includes(x3) || shipArray.includes(x4)) {
+        randomCoords(num);
+      }
+      // eslint-disable-next-line max-len
+      if (num === 5 && !row1.includes(x) && !row2.includes(x) && !row3.includes(x) && !row4.includes(x)) {
+        for (let i = 0; i < num; i++) {
+          randomArray.push(y + i);
+        }
+      } else if (num === 4 && !row2.includes(x) && !row3.includes(x) && !row4.includes(x)) {
+        for (let i = 0; i < num; i++) {
+          randomArray.push(y + i);
+        }
+      } else if (num === 3 && !row3.includes(x) && !row4.includes(x)) {
+        for (let i = 0; i < num; i++) {
+          randomArray.push(y + i);
+        }
+      } else if (num === 2 && !row4.includes(x)) {
+        for (let i = 0; i < num; i++) {
+          randomArray.push(y + i);
+        }
+      } else {
+        randomCoords(num);
+      }
+    } else if (z === 1) {
+      const x1 = y + 10;
+      const x2 = y + 20;
+      const x3 = y + 30;
+      const x4 = y + 40;
+      // eslint-disable-next-line max-len
+      if (shipArray.includes(x) || shipArray.includes(x1) || shipArray.includes(x2) || shipArray.includes(x3) || shipArray.includes(x4)) {
+        randomCoords(num);
+      }
+      // eslint-disable-next-line max-len
+      if (num === 5 && !column1.includes(x) && !column2.includes(x) && !column3.includes(x) && !column4.includes(x)) {
+        for (let i = 0; i < num; i++) {
+          const q = i * 10;
+          randomArray.push(y + q);
+        }
+      // eslint-disable-next-line max-len
+      } else if (num === 4 && !column2.includes(x) && !column3.includes(x) && !column4.includes(x)) {
+        for (let i = 0; i < num; i++) {
+          const q = i * 10;
+          randomArray.push(y + q);
+        }
+      } else if (num === 3 && !column3.includes(x) && !column4.includes(x)) {
+        for (let i = 0; i < num; i++) {
+          const q = i * 10;
+          randomArray.push(y + q);
+        }
+      } else if (num === 2 && !column4.includes(x)) {
+        for (let i = 0; i < num; i++) {
+          const q = i * 10;
+          randomArray.push(y + q);
+        }
+      } else {
+        randomCoords(num);
+      }
+    }
+    shipArray.push(randomArray);
+    console.log(shipArray);
+    return randomArray;
+  };
+
   // make new ship using factorial function
-  const carrier = ship('carrier', 5, '', false, [1, 2, 3, 4, 5]);
-  const battleship = ship('battleship', 4, '', false, [6, 16, 26, 36]);
-  const destroyer = ship('destroyer', 3, '', false, [90, 91, 92]);
-  const submarine = ship('submarine', 3, '', false, [50, 60, 70]);
-  const patrolBoat = ship('patrol-boat', 2, '', false, [39, 49]);
+  const carrier = ship('carrier', 5, '', false, randomCoords(5));
+  const battleship = ship('battleship', 4, '', false, randomCoords(4));
+  const destroyer = ship('destroyer', 3, '', false, randomCoords(3));
+  const submarine = ship('submarine', 3, '', false, randomCoords(3));
+  const patrolBoat = ship('patrol-boat', 2, '', false, randomCoords(2));
 
   // place ship in gameArray
   const placeShip = (shipName, player) => {
@@ -173,25 +260,25 @@ const gameboard = () => {
 
 const player = () => {
   const playerOne = gameboard();
+  playerOne.createGrid(1);
   const createPlayer = () => {
-    playerOne.createGrid(1);
-    playerOne.placeShip('submarine', 1);
-    playerOne.placeShip('carrier', 1);
-    playerOne.placeShip('battleship', 1);
-    playerOne.placeShip('patrol-boat', 1);
-    playerOne.placeShip('destroyer', 1);
+    setTimeout(() => {
+      playerOne.placeShip('submarine', 1);
+      playerOne.placeShip('carrier', 1);
+      playerOne.placeShip('battleship', 1);
+      playerOne.placeShip('patrol-boat', 1);
+      playerOne.placeShip('destroyer', 1);
+    }, 1000);
   };
   const computerOne = gameboard();
+  computerOne.createGrid(2);
   const createComputer = () => {
-    computerOne.createGrid(2);
     computerOne.placeShip('submarine');
     computerOne.placeShip('carrier');
     computerOne.placeShip('battleship');
     computerOne.placeShip('patrol-boat');
     computerOne.placeShip('destroyer');
   };
-
-  let turn = 1;
 
   const computerArray = [];
   for (let i = 0; i < 100; i++) {
@@ -201,17 +288,13 @@ const player = () => {
   const hitPlayer = () => {
     const x = computerArray.splice(Math.floor(Math.random() * computerArray.length), 1);
     playerOne.receiveAttack(playerOne.gameArray[x], x, 1);
-    turn = 1;
   };
 
   const hitComputer = (coords, num, x) => {
-    if (turn === 1) {
-      computerOne.receiveAttack(coords, num, x);
-      turn = 2;
-      setTimeout(() => {
-        hitPlayer();
-      }, '500');
-    }
+    computerOne.receiveAttack(coords, num, x);
+    setTimeout(() => {
+      hitPlayer();
+    }, '500');
   };
 
   return {
